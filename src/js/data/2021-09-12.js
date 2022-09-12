3bfe985491b463617ae88525a6fcbe04c11aebc8
mqole <113324899+mqole@users.noreply.github.com>
2022-09-12 18:33:59 +1000

    WOOOOO prosecutors & prsk

---
+++ src/js/data/2021-09-12.js
@@ -0,0 +1,313 @@
+dataSetVersion = "2022-09-12"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Pick your poison",
+    key: "poison",
+    tooltip: "uncheck this and youll just get Everything",
+    sub: [
+      { name: "Ace Attorney Prosecutors", tooltip: "Just the ones that matter", key: "AAPro" },
+      { name: "Miku bitches", tooltip: "Project sekai, specifically", key: "PRSK" },
+      // add more here, using above format
+    ]
+  },
+  // filters go here btw. if i ever decide on having them
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Winston Payne",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Miles Edgeworth",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Manfred von Karma",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Lana Skye",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Franziska von Karma",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Godot",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Sebastian Debeste",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Blaise Debeste",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Klavier Gavin",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Gaspen Payne",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Simon Blackquill",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Nahyuta Sadmadhi",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Ga'ran Sigatar Khura'in",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Taketsuchi Auchi",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Barok van Zieks",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Jules' little spoiler boy",
+    img: "https://i.imgur.com/E3cWvFt.png",
+    opts: {
+      poison: ["AAPros"]
+    }
+  },
+  {
+    name: "Hatsune Miku",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/a/a9/Miku_1_thumbnail.png/144px-Miku_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Kagamine Rin",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/1/15/Rin_1_thumbnail.png/144px-Rin_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Kagamine Len",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/d/df/Len_1_thumbnail.png/144px-Len_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Megurine Luka",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/1/17/Luka_1_thumbnail.png/144px-Luka_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "MEIKO",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/6/67/MEIKO_1_thumbnail.png/144px-MEIKO_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "KAITO",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/a/aa/KAITO_1_thumbnail.png/144px-KAITO_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Ichika Hoshino",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/1/12/Ichika_1_thumbnail.png/144px-Ichika_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Saki Tenma",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/4/4a/Saki_1_thumbnail.png/144px-Saki_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Honami Mochizuki",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/9/97/Honami_1_thumbnail.png/144px-Honami_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Shiho Hinomori",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/1/1b/Shiho_1_thumbnail.png/144px-Shiho_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Minori Hanasato",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/7/78/Minori_1_thumbnail.png/144px-Minori_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Haruka Kiritani",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/4/44/Haruka_1_thumbnail.png/144px-Haruka_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Airi Momoi",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/2/26/Airi_1_thumbnail.png/144px-Airi_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Shizuku Hinomori",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/f/f5/Shizuku_1_thumbnail.png/144px-Shizuku_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Kohane Azusawa",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/b/b8/Kohane_1_thumbnail.png/144px-Kohane_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "An Shiraishi",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/b/b3/An_1_thumbnail.png/144px-An_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Akito Shinonome",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/0/0d/Akito_1_thumbnail.png/144px-Akito_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Toya Aoragi",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/3/31/Toya_1_thumbnail.png/144px-Toya_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Tsukasa Tenma",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/9/9c/Tsukasa_1_thumbnail.png/144px-Tsukasa_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Emu Otori",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/a/ad/Emu_1_thumbnail.png/144px-Emu_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Nene Kusanagi",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/b/b8/Nene_1_thumbnail.png/144px-Nene_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Rui Kamishiro",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/5/51/Rui_1_thumbnail.png/144px-Rui_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Kanade Yoisaki",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/a/a6/Kanade_1_thumbnail.png/144px-Kanade_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Mafuyu Asahina",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/5/50/Mafuyu_1_thumbnail.png/144px-Mafuyu_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Ena Shinonome",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/2/22/Ena_1_thumbnail.png/144px-Ena_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+  {
+    name: "Mizuki Akiyama",
+    img: "https://static.miraheze.org/projectsekaiwiki/thumb/d/d0/Mizuki_1_thumbnail.png/144px-Mizuki_1_thumbnail.png",
+    opts: {
+      poison: ["PRSK"]
+    }
+  },
+];
